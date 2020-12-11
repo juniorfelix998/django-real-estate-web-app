8ce96993c4fce41a5e167d37da60622b2af095be
juniorfelix998 <juniorfelix998@gmail.com>
2020-12-11 12:42:25 +0300

    added comments

--- btre/settings.py
+++ btre/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static' #added
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'btre/static'
-]
+] #added
 
 # Media Folder Settings
 
